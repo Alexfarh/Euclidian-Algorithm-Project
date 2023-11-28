1c8c4161f8ada7db0665e4c4663ee0267a116191
Alexfarh <138962977+Alexfarh@users.noreply.github.com>
2023-11-28 16:16:20 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 # Use Recursion To AutoMate Euclid's Algorithm
-def Euclid(x:int, y:int) -> int:
-    pass
+def euclid(x: int, y: int) -> int:
+    if x > y:
+        remainder = x % y
+        gcd = y
+    else:
+        remainder = y % x
+        gcd = x
+    if remainder != 0:
+        gcd = euclid(gcd, remainder)
+    return gcd
+
+
+print(euclid(615, 810))
+# 15
